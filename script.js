d1b6433e5bbd46466d63960273357aa6b8d2567b
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-08-23 20:30:14 +0800

    semi-completed UI

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-console.log("Hello")
-
 let gameOver = false;
 let choices = ["rock", "paper", "scissor"];
 let winningMessage = "";
-let userScore = 4;
+let userScore = 0;
 let computerScore = 0;
 let winner;
 
 
+let player__score = document.getElementById("player__score");
+let computer__score = document.getElementById("computer__score")
+let result__text = document.getElementById("result__box__text")
 let rockBtn = document.getElementById("rock__btn");
 let paperBtn = document.getElementById("paper__btn");
 let scissorBtn = document.getElementById("scissor__btn");
@@ -37,14 +38,14 @@ function playRound(userChoice, computerChoice) {
     if (userChoice == computerChoice) {
         winningMessage = "It's a tie!"
     } else if ((userChoice == "rock" && computerChoice == "paper") || (userChoice == "paper" && computerChoice == "scissor") || (userChoice == "scissor" && computerChoice == "rock")) {
-        winningMessage = `You lose ${computerChoice} beats ${userChoice}`;
+        winningMessage = `You lose! ${computerChoice} beats ${userChoice}`;
         computerScore += 1;
     } else if ((userChoice == "rock" && computerChoice == "scissor") || (userChoice == "paper" && computerChoice == "rock") || (userChoice == "scissor" && computerChoice == "paper")) {
-        winningMessage = `You win ${userChoice} beats ${computerChoice}`;
+        winningMessage = `You win! ${userChoice} beats ${computerChoice}`;
         userScore += 1;
     }
 
-    console.log(winningMessage);
+    result__text.textContent = winningMessage;
 }
 
 
@@ -59,7 +60,7 @@ function game(choice) {
     }
     if(gameOver) {
         console.log(winningMessage);
-        disableButtons(buttons);
+        disableButtons(buttons)
     } 
 }
 
@@ -68,8 +69,22 @@ function logScore() {
     console.log(`User score is ${userScore}`)
     console.log(`computer score is ${computerScore}`)
     console.log("-----------------------------")
+    player__score.textContent = userScore;
+    computer__score.textContent = computerScore;
+
 }
 
 function disableButtons(buttons) {
     Array.from(buttons).map(btn => btn.disabled = true);
+    document.getElementById("reset__btn").disabled = false;
 }
+
+document.getElementById("reset__btn").addEventListener("click", function() {
+    userScore = 0;
+    computerScore = 0;
+    result__text.textContent = "New game. Take your pick!"
+    player__score.textContent = userScore;
+    computer__score.textContent = computerScore;
+    Array.from(buttons).map(btn => btn.disabled = false);
+    gameOver = false;
+})
